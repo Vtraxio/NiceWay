4d2e445d995796241ed4d5d86f610cb516861de5
Vtraxio <55654178+Vtraxio@users.noreply.github.com>
2024-12-23 18:55:48 +0100

    Format

--- NiggaWay/Form1.cs
+++ NiggaWay/Form1.cs
@@ -4,8 +4,9 @@ using System.Text.Json;
 namespace NiggaWay {
 	public partial class Form1 : Form {
 		private List<City> _cities = [];
-        // Bez tej zmiennej przed wybraniem punktu nadal by na mapie narysowa³o po³¹czenia, gdy nie jest nic przesortowane
-        private bool       _ready;
+
+		// Bez tej zmiennej przed wybraniem punktu nadal by na mapie narysowa³o po³¹czenia, gdy nie jest nic przesortowane
+		private bool _ready;
 
 		public Form1() {
 			InitializeComponent();
@@ -20,7 +21,7 @@ namespace NiggaWay {
 			_cities = JsonSerializer.Deserialize<List<JsonPoint>>(text)!.Select(x => new City(new PointF(x.x, x.y), false, null)).ToList();
 
 			lengthLbl.Text = @"Wybierz jakiœ punkt";
-			_ready          = false;
+			_ready         = false;
 			Refresh();
 		}
 
@@ -28,7 +29,7 @@ namespace NiggaWay {
 			// Punty s¹ obliczane w stylu bubble sorta, dlatego punkt pocz¹tkowy trzeba daæ na sam pocz¹tek a wywaliæ to, co wtedy tam by³o 
 			ListUtils.Swap(_cities, 0, origin);
 			// Punkt, dla którego w³aœnie liczymy najbli¿sze miasto
-			var current  = 0;
+			var current     = 0;
 			var totalLength = 0d;
 
 			var sw = new Stopwatch();
@@ -39,11 +40,11 @@ namespace NiggaWay {
 
 				var closest = _cities
 							  .Select((city, i) => (city, i)) // Wyci¹gamy indeks, aby po zalezieniu miasta móc je potem zamieniæ
-                              .Skip(current + 1) // Pomijamy miasta, które s¹ ju¿ obliczone oraz obecne
-                              .Select(data => {
-                                  // Obliczamy odleg³oœæ miêdzy dwoma miastami i dodajemy je do danych miasta, aby u¿yæ w nastêpnym kroku.
-                                  // Nie mo¿na tego zrobiæ w MinBy, bo musimy potem u¿yæ d³ugoœci do obliczenia ca³kowitej d³ugoœci trasy
-                                  var length = Math.Sqrt(Math.Pow(node.Point.X - data.city.Point.X, 2) + Math.Pow(node.Point.Y - data.city.Point.Y, 2));
+							  .Skip(current + 1)              // Pomijamy miasta, które s¹ ju¿ obliczone oraz obecne
+							  .Select(data => {
+								  // Obliczamy odleg³oœæ miêdzy dwoma miastami i dodajemy je do danych miasta, aby u¿yæ w nastêpnym kroku.
+								  // Nie mo¿na tego zrobiæ w MinBy, bo musimy potem u¿yæ d³ugoœci do obliczenia ca³kowitej d³ugoœci trasy
+								  var length = Math.Sqrt(Math.Pow(node.Point.X - data.city.Point.X, 2) + Math.Pow(node.Point.Y - data.city.Point.Y, 2));
 								  return new { city = new City(data.city.Point, data.city.Selected, length), data.i };
 							  })
 							  .MinBy(p => p.city.DistanceToNext);
@@ -53,7 +54,7 @@ namespace NiggaWay {
 
 				current++;
 				// Tu mo¿e byæ null, wiêc robimy ?? 0, aby visual zamkn¹ morde.
-				totalLength    += closest.city.DistanceToNext ?? 0;
+				totalLength += closest.city.DistanceToNext ?? 0;
 			}
 
 			sw.Stop();
@@ -65,9 +66,9 @@ namespace NiggaWay {
 
 		private void pictureBox1_Click(object sender, EventArgs e) {
 			var mouse = pictureBox1.PointToClient(MousePosition);
-            // W json jest od -100 do 100 a w pictureBoxie od 0 do 500 wiêc trzeba to przekonwertowaæ
-            var x     = GeneralUtils.ConvertRange(0, 500, -100, 100, mouse.X);
-			var y     = GeneralUtils.ConvertRange(0, 500, -100, 100, mouse.Y);
+			// W json jest od -100 do 100 a w pictureBoxie od 0 do 500 wiêc trzeba to przekonwertowaæ
+			var x = GeneralUtils.ConvertRange(0, 500, -100, 100, mouse.X);
+			var y = GeneralUtils.ConvertRange(0, 500, -100, 100, mouse.Y);
 
 			for (var i = 0; i < _cities.Count; i++) {
 				var city = _cities[i];
